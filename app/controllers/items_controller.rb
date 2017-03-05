530379c8a15595c22fe6ae652995e2b4be0d773c
icebear <wilsontan.2088@gmail.com>
2017-03-06 02:13:56 +0800

    Test proof of concept that scrapping works

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def new
+    # modal popup + ajax?
     @item = Item.new
   end
 
@@ -32,12 +33,23 @@ class ItemsController < ApplicationController
   end
 
   def scrape
-    
+    @items = Item.all
+
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
+
+    @items.each do |item|
+      doc = Nokogiri::HTML(open(item.link, 'User-Agent' => user_agent))
+      item.result = doc.css(item.selector).text
+
+      item.save
+    end
+
+    redirect_to items_path
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:link, :selector, :extra)
+    params.require(:item).permit(:link, :selector, :extra, :title)
   end
 end
