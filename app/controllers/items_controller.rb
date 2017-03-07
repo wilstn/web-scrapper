1e8d2aa3c272a1d93778e6fd0ca91ede0b8546d8
icebear <wilsontan.2088@gmail.com>
2017-03-07 10:25:50 +0800

    Shift bulk of scrape method from controller to model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,23 +47,27 @@ class ItemsController < ApplicationController
   def scrape
     @items = Item.all
 
-    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0"
-
     @items.each do |item|
-      doc = Nokogiri::HTML(open(item.link, 'User-Agent' => user_agent))
-
-      preprocess = doc.css(item.selector)
-
-      to_be_saved = "<ol>"
-      preprocess.each do |something|
-        to_be_saved += "<li>#{something.text}</li>"
-      end
-      to_be_saved += "</ol>"
-
-      item.result = to_be_saved
-
-      item.save
+      item.scrape
     end
+
+    # user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0"
+
+    # @items.each do |item|
+    #   doc = Nokogiri::HTML(open(item.link, 'User-Agent' => user_agent))
+    #
+    #   preprocess = doc.css(item.selector)
+    #
+    #   to_be_saved = "<ol>"
+    #   preprocess.each do |something|
+    #     to_be_saved += "<li>#{something.text}</li>"
+    #   end
+    #   to_be_saved += "</ol>"
+    #
+    #   item.result = to_be_saved
+    #
+    #   item.save
+    # end
 
     redirect_to items_path
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+
+  def scrape
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0"
+
+    doc = Nokogiri::HTML(open(self.link, 'User-Agent' => user_agent))
+
+    selected_object = doc.css(self.selector)
+
+    to_be_saved = "<ol>"
+
+    selected_object.each do |row|
+      to_be_saved += "<li>#{row.text}</li>"
+    end
+
+    to_be_saved += "</ol>"
+
+    self.result = to_be_saved
+
+    self.save
+  end
 end
